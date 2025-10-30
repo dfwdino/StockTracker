8cb37d9777907af04e69b4ab7ee3de376bc2116b
shane n <shane.newsom@gmail.com>
2025-10-30 15:11:53 -0500

    Fix how you enter numbers.

--- StockTracker/obj/Debug/net8.0/StockTracker.AssemblyInfo.cs
+++ StockTracker/obj/Debug/net8.0/StockTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c5d61d5918ee293db32aa1f44ec2bb762a28e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb337efad50b26e26d80319058e0e10e401e56bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
