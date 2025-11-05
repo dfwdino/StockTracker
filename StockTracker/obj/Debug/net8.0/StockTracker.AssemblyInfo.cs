26c1ea27091336e8cca61c00215fcfe4002bd980
shane n <shane.newsom@gmail.com>
2025-11-05 13:02:10 -0600

    asdfasd

--- StockTracker/obj/Debug/net8.0/StockTracker.AssemblyInfo.cs
+++ StockTracker/obj/Debug/net8.0/StockTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3382c5aba6db6f57b5e5a14c9fb6fe00dd1c7e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e71a0ef9c148ea8603c7d6d007d61ad0ce407e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
